518907b4d68a566cb93643fe6a1680a444366d87
Artyom <acidflora@onil-ato.ru>
2020-08-04 11:18:02 +0300

    Revert "????"
    This reverts commit 8efcd325db5b34d7e329a521116760acb7c5250f.

---
+++ Fingerprint Scanner Service/Interfaces/IObservable.cs
@@ -0,0 +1,10 @@
+﻿namespace Fingerprint_Scanner_Service
+{
+    interface IObservable
+    {
+        void RegisterObserver(IObserver o);//Части реализации интерфейса
+        void RemoveObserver(IObserver o);//Части реализации интерфейса
+        void NotifyObserversAboutConnection();//Части реализации интерфейса
+        void NotifyObserversAboutUpdate();//Части реализации интерфейса
+    }
+}
---
+++ Fingerprint Scanner Service/Interfaces/IObserver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Fingerprint_Scanner_Service
+{
+    interface IObserver
+    {
+        void UpdateStatusServer(Object ob);//Части реализации интерфейса
+        void UpdateStatusFinger(Object ob);//Части реализации интерфейса
+    }
+}
